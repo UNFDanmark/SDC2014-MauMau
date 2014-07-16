0391476202869e57b72471112b04521821a3b38e
sdc <github@software.unf.dk>
2014-07-16 09:27:17 +0200

    Added base classes for UI

---
+++ src/dk/unf/MauMau/InputEvent.java
@@ -0,0 +1,18 @@
+package dk.unf.MauMau;
+
+/**
+ * Created by sdc on 7/16/14.
+ */
+public class InputEvent {
+
+    public final int x;
+    public final int y;
+    public final int pointer;
+
+    public InputEvent(int x, int y, int pointer) {
+        this.x = x;
+        this.y = y;
+        this.pointer = pointer;
+    }
+
+}
---
+++ src/dk/unf/MauMau/ui/Element.java
@@ -0,0 +1,47 @@
+package dk.unf.MauMau.ui;
+
+/**
+ * Created by sdc on 7/16/14.
+ */
+public abstract class Element {
+
+    protected int id;
+    protected int x;
+    protected int y;
+    protected int width;
+    protected int height;
+
+
+    public Element(int id, int x, int y, int width, int height) {
+        this.id = id;
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public boolean inside(int u, int v) {
+        return (x > u && x+width < u && y > v && y < v+height);
+    }
+
+}
